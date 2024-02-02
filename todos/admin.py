1d2b9a5748160410d0c5804adc3764fd6f19b3d0
Shashi Bhagat <skujur871@gmail.com>
2024-02-03 03:15:35 +0530

    added new realtions and worked on admin config file

--- todos/admin.py
+++ todos/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 
-from .models import Todo
+from .models import Todo, Author, Address
 # Register your models here.
+
 
 class TodoAdmin(admin.ModelAdmin):
     readonly_fields = ("completed_at",)
     list_filter = ("completed_at", "progress", "status")
-    list_display = ("title", "description", "progress", "updated_at", "completed_at")
+    list_display = ("title", "description", "progress",
+                    "updated_at", "completed_at", 'author')
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'address')
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('street', 'city', 'postal_code', 'country')
+
 
 admin.site.register(Todo, TodoAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address, AddressAdmin)
--- todos/views.py
+++ todos/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect, Http404
 from django.template.loader import render_to_string
+from datetime import date
 
-from .models import Todo
+from .models import Todo, Author
 
 # Create your views here.
 
 
 def get_todos(request):
     todos = Todo.objects.all()
+    shashi = Author.objects.get(pk=1)
+    print(shashi)
+    todos_weget = shashi.todos.filter(progress__gt=9)
+    print(todos_weget)
     return HttpResponse(render(request=request,
                                template_name="todos/todo_list.html",
                                context={"todos_list": todos, "selected_nav": 1, "name": "Todo App"}))
@@ -34,6 +39,7 @@ def invalid_response(request):
 
 def update_task(request, task_id):
     todo = get_object_or_404(klass=Todo, pk=task_id)
+    todo.updated_at = date.today()
     return HttpResponse(render(request=request,
                                template_name="todos/create_todos.html",
                                context={
@@ -44,5 +50,6 @@ def update_task(request, task_id):
 
 def delete_task(request, task_id):
     todo = get_object_or_404(klass=Todo, pk=task_id)
-    todo.delete()
+    print(todo.author.get_full_name())
+    # todo.delete()
     return redirect("todo_list")
