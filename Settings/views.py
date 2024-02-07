32bef95b0441700795a5a85812b47762fedeaa6c
Shashi Bhagat <skujur871@gmail.com>
2024-02-08 00:01:52 +0530

    setting up new app in the app names settings and used classed view

--- Settings/views.py
+++ Settings/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
 
-def setting_page(request):
-    return render(request=request,
+class SettingsView(View):
+    def get(self, request):
+        
+        return render(request=request,
                   template_name="settings.html",
                   context={
                       
                   })
+
